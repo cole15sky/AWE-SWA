6d2ec544329201d2afb148a1010c36c8e2b3d135
Sky Blue <dawadiaakash@gmail.com>
2024-10-28 11:57:23 +0545

    style: apply consistent styling across the entire page

--- pages/agenda.vue
+++ pages/agenda.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
         <Navbar />
-        <div class="flex justify-center w-screen min-h-50 pt-20 pl-20">
-            <div class="w-1/6 p-4">
-                <ul class="space-y-1 mt-9">
+        <div class="flex justify-center w-screen min-h-50 pt-20 pr-18">
+            <div class="w-1/7 p-5">
+                <ul class="space-y-1 ml-5 mt-9">
                     <li v-for="(item, index) in uniqueArray.slice(0, 4)" :key="index">
                         <button @click="selectType(item)"
-                            class="relative w-full  text-xl p-2 flex justify-between items-center" :class="{
+                            class="relative w-[140px]  text-md p-3 b-2  whitespace-normal flex justify-between items-center" :class="{
                                 'bg-gradient-to-r from-[#00012D] to-[#03025f] text-white': selectedType === item,
-                                'bg-sky-100 text-black': selectedType !== item
+                                'bg-gray-100 text-black': selectedType !== item
                             }">
-                            <span>{{ toTitleCase(item) }}</span>
+                            <span class="font-semibold ">{{ toTitleCase(item) }}</span>
 
                             <div v-if="selectedType === item"
                                 class="absolute right-[-15px] top-1/2 transform -translate-y-1/2 border-t-[10px] border-b-[10px] border-l-[15px] border-t-transparent border-b-transparent border-l-[#03025f]">
@@ -20,17 +20,17 @@
                 </ul>
             </div>
 
-            <div class="w-2/4 p-15">
+            <div class="w-3/5 p-15 b-10 mr-15 shadow-xl">
                 <div v-if="loading" class="text-center text-gray-500">Loading data...</div>
                 <div v-if="error" class="text-center text-red-500">Error: {{ error }}</div>
                 <div>
 
                     <!-- Display items filtered by selectedType and selectedDate -->
-                    <div class="flex justify-end space-x-4">
-                        <ul class="flex space-x-1 ">
+                    <div class="flex justify-end space-x-4 ">
+                        <ul class="flex space-x-1 shadow-md">
                             <li v-for="(date, index) in uniqueArrayDate.slice(0, 4)" :key="index">
                                 <button @click="selectDateType(date)"
-                                    class="relative w-full text-xl p-3  justify-between" :class="{
+                                    class="relative w-full text-xl p-3  justify-between " :class="{
                                         'bg-gradient-to-r from-[#00012D] to-[#03025f] text-white': selectedDateType === date,
                                         ' shadow-lg text-black': selectedDateType !== date
                                     }">
@@ -40,12 +40,12 @@
                         </ul>
                     </div>
                     <!-- Display filtered data with profile images -->
-                    <div v-for="(item, index) in filteredData" :key="index" class="p-6 shadow-md">
+                    <div v-for="(item, index) in filteredData" :key="index" class="p-6 shadow-sm ">
                         <h3 class="text-[#00012D] font-bold">{{ item.title }}</h3>
-                        <p>{{ item.description }}</p>
+                        <p class=" text-gray-400">{{ item.description }}</p>
                         <div class="flex flex-row  items-center gap-2">
-                            <UIcon name="hugeicons:clock-01" class="w-4 h-4 space-y-4" />
-                            <p> {{ formatTime(item.startDate) }} - {{ formatTime(item.endDate) }}</p>
+                            <UIcon name="hugeicons:clock-01" class="w-3 h-3 space-y-4" />
+                            <p class="text-sky-300 text-xs "> {{ formatTime(item.startDate) }} - {{ formatTime(item.endDate) }}</p>
                         </div>
 
                         <!-- Rendering speaker profiles -->
@@ -73,7 +73,7 @@ const data = ref(null);
 const error = ref(null);
 const loading = ref(true);
 const selectedType = ref('panelDiscussions');
-const selectedDateType = ref(null);
+const selectedDateType = ref('null');
 
 const uniqueArray = ref([]);
 const uniqueArrayDate = ref([]);
